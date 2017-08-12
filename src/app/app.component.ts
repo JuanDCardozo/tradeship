946e9b86b60d4c655b528da3a3be3ceb83733399
Matthew Kim <madkim@ucsc.edu>
2017-08-12 01:03:03 -0700

    Added ngDoCheck() to check for change in loggedIn boolean.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { OnInit,Component } from '@angular/core';
+import { OnInit, DoCheck, Component } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Router,ActivatedRoute, Params} from '@angular/router';
 import { TokenService } from './token-service/token.service';
@@ -24,12 +24,16 @@ export class AppComponent{
       this.tokenService.storeToke(accessToken);
       //DEBUG
       // let testToken = this.tokenService.retrieveToke();
-      //Check if loggedIn
-      this.loggedIn = this.tokenService.isLoggedIn();
       //DEBUG
       // console.log('testToken: '+ testToken);
       console.log('LoggedIn: '+ this.loggedIn);
     })
+  }
+
+  //Used to check for changes that Angular won't detect
+  ngDoCheck() {
+    //Check if loggedIn
+    this.loggedIn = this.tokenService.isLoggedIn();
   }
 
   Logout(){
